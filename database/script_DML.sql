9b201514220058b896ce6ab91f272c95737ae28e
Julio Lopez <julioneo95@hotmail.com>
2023-11-22 00:43:52 -0300

    Add script DB/datos

---
+++ database/script_DML.sql
@@ -0,0 +1,18 @@
+
+select * from nutricionista;
+select * from consultante;
+select * from receta;
+select * from ingrediente;
+select * from receta_ingrediente;
+
+
+DROP TABLE IF EXISTS nutricionista;
+DROP TABLE IF EXISTS consultante;
+DROP TABLE IF EXISTS receta;
+DROP TABLE IF EXISTS ingrediente;
+DROP TABLE IF EXISTS receta_ingrediente;
+DROP TABLE IF EXISTS nutricionista_consultante;
+DROP TABLE IF EXISTS afeccion;
+DROP TABLE IF EXISTS consultantes_recetas;
+DROP TABLE IF EXISTS consultante_afeccion;
+
