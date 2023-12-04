2d17fe8c01f62395fd31204e09501daf98c0edee
Julio Lopez <julioneo95@hotmail.com>
2023-12-04 16:53:15 -0300

    feat: login almost done

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,33 @@
-export function App(){
+export function App() {
   return (
-    <div className=" text-center h-screen bg-red-900 text-white">
-      <h1 className="text-3xl">Bienvenido nutricionista</h1>
+
+    <div className="container max-w-2xl bg-white rounded-2xl border-2 p-10 my-28 mx-auto space-x-4 space-y-4">
+
+      <h1 className="text-3xl text-center font-semibold">Bienvenido nutricionista</h1>
+
+      <p className="font-semibold">Este es un espacio dedicado a profesionales de la nutrición para gestionar y mejorar la salud de sus pacientes.</p>
+
+      <div className="space-x-4 space-y-4">
+
+        <div className="flex justify-center">
+          <label form="email" className="font-semibold">Email:</label>
+          <input type="email" id="email" name="email" required className="rounded-md border-2 border-solid border-gray-300" />
+        </div>
+
+        <div className="flex justify-center">
+          <label form="password" className="font-semibold">Contraseña:</label>
+          <input type="password" id="password" name="password" required className="rounded-md border-2 border-solid border-gray-300" />
+        </div>
+
+      </div>
+
+      <div className="flex justify-center space-x-4">
+
+        <button type="submit" className="p-3 bg-smoke rounded-2xl border-2 border-magenta ">Ingresar al Sistema</button>
+        <button type="button" className="p-3 bg-magenta rounded-2xl border-2 border-solid text-gris">Registrarse</button>
+      </div>
+
+
     </div>
   )
 }
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,16 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      azul_magenta: '#6b45a8', //botones principales
-      tea_green: '#d7f4c3', //barra de busqueda
-      search_icon_color:'#8fce67', //color del icono buscador
-      //quick_silver:'#a0a0a0',
-      anti_flash_white:'#f2f3f0',//color de fondo de pagina // color sombra campos input
-      whiteSmoke:'#f5f5f5', //color fondo campos input
-
-    }
-    , extend: {},
+    
+    extend: {
+      colors:{
+        
+        magenta: '#6b45a8', //botones principales
+        verdeClaro: '#d7f4c3', //barra de busqueda
+        verde:'#8fce67', //color del icono buscador
+        //quick_silver:'#a0a0a0',
+        gris:'#f2f3f0',//color de fondo de pagina // color sombra campos input
+        grisOscuro:'#f5f5f5', //color fondo campos input
+      }
+    },
   },
   plugins: [],
 }
