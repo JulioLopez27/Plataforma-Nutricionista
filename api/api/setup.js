1c20a5eae96e38fc5e6427fb9ee76c99cf28f5ca
Julio Lopez <julioneo95@hotmail.com>
2023-12-17 22:52:47 -0300

    Change location folder for server config

---
+++ api/api/setup.js
@@ -0,0 +1,14 @@
+//configuración necesaria para que el routeamiento 
+//funcione correctamente.
+import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
+import cors from '@koa/cors'
+import { router } from './router.js'
+
+
+export const app = new Koa()
+
+app.use(cors())
+app.use(bodyParser())
+app.use(router.routes())
+app.use(router.allowedMethods())
