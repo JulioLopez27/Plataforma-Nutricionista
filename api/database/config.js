9cfe44bf9bcbbf5b18b0ff3b965460b95911401c
Julio Lopez <julioneo95@hotmail.com>
2023-12-17 22:52:01 -0300

    database configuration file

--- api/database/config.js
+++ api/database/config.js
@@ -1,22 +1,19 @@
-//conexión con la database
 import Mysql from 'mysql2'
+
 
 //exporto la conexion entre el servidor y la db para 
 //cuando se cree la instancia del servidor ya ejecute este script
+
 export const connection = Mysql.createConnection({
-    host: "localhost",
+    host: 'localhost',
     user: 'JulioLopez',
-    password: '!Julio29329733',
-    port: 3306,
+    password:'!Julio29329733',
+    port:3306,
     database: 'nutricionistas'
 })
 
 
-
-// connection.connect(function (err) {
-//     if (err) throw err;
-//     connection.query("Select * from nutricionista",  (err, result, field) => {
-//         if (err) throw err
-//         console.log(result);
-//     })
-// })
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
