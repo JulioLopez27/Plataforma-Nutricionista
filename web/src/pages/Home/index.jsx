b97ea5691ee6f3487337fc7635e65902adbed617
Julio Lopez <julioneo95@hotmail.com>
2024-01-11 06:07:38 -0300

    Add: formik en inputs / formik para enviar los datos al back/ validacion de inputs(yup)

--- web/src/pages/Home/index.jsx
+++ web/src/pages/Home/index.jsx
@@ -1,9 +1,41 @@
 
+import axios from 'axios'
+import * as yup from 'yup'
+import { useFormik } from 'formik'
+
 import { Input } from '~/components'
 
-
+const validationSchema = yup.object().shape({
+  email: yup.string().required('Campo obligatorio').email('Ingrese un formato valido').trim(),
+  password: yup.string().required('Campo obigatorio').trim()
+})
 
 export function Home() {
+
+  const formik = useFormik({
+    onSubmit: async (values) => {
+
+      try {
+        const res = await axios({
+          method: 'post',
+          baseURL: 'http://localhost:3000',
+          url: '/login',
+          data: values
+        })
+
+        console.log("🚀 ~ file: index.jsx:20 ~ onSubmit: ~ res:", res.data)
+      } catch (error) {
+        console.log('Error del servidor', error);
+      }
+    },
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema
+  })
+
+
   return (
 
     <div className="container max-w-2xl bg-white rounded-2xl border-2 p-8 my-24 mx-auto space-x-4 ">
@@ -13,7 +45,7 @@ export function Home() {
 
         <p className="font-semibold">Este es un espacio dedicado a profesionales de la nutrición para gestionar y mejorar la salud de sus pacientes.</p>
 
-        <form className="space-y-4 mt-3">
+        <form className="space-y-4 mt-3" onSubmit={formik.handleSubmit}>
 
           <Input
             autoComplete="off"
@@ -21,7 +53,10 @@ export function Home() {
             name="email"
             label="Correo electrónico "
             placeholder="ingrese su correo"
-
+            error={formik.touched.email && formik.errors.email}
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
 
           <Input
@@ -29,17 +64,19 @@ export function Home() {
             name="password"
             label="Constraseña"
             placeholder="Ingrese su contraseña"
+            error={formik.touched.password && formik.errors.password}
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
 
+          <div className="flex justify-center space-x-4 mt-4">
+
+            <button type='submit' className="text-center p-3 bg-smoke rounded-xl border-2 border-magenta ">Ingresar al Sistema</button>
+            <a href="/signup" className="text-center p-3 bg-magenta rounded-xl border-2 border-solid text-gris">Registrarse</a>
+          </div>
+
         </form>
-
-
-
-        <div className="flex justify-center space-x-4 mt-4">
-
-          <button className="text-center p-3 bg-smoke rounded-xl border-2 border-magenta ">Ingresar al Sistema</button>
-          <a href="/signup" className="text-center p-3 bg-magenta rounded-xl border-2 border-solid text-gris">Registrarse</a>
-        </div>
 
       </main>
 
