f1fb57ace16290c6d4ce1fb51af8a47f1ca0082f
Julio Lopez <julioneo95@hotmail.com>
2024-01-29 20:07:10 -0300

    Add new colors

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,11 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    
+
     extend: {
-      colors:{
+      colors: {
         verde_oscuro: '#0B5B37', //color de relleno de los botones
-        verde_claro:'#229C44', // color para encabezado
+        verde_claro: '#229C44', // color para encabezado
+        azul_titulo: '#111827',
+        gris_texto:'#404652',
       }
     },
   },
