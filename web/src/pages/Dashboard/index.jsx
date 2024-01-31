e2500a77dbdd80540e217dfcf89a8afd53006e59
Julio Lopez <julioneo95@hotmail.com>
2024-01-30 22:59:06 -0300

    Add: import for Header/NavBar

--- web/src/pages/Dashboard/index.jsx
+++ web/src/pages/Dashboard/index.jsx
@@ -1,5 +1,5 @@
 
-import {Recepies_Cards} from "../../components/Recepies_Cards"
+import { Recepies_Cards, Header, NavBar } from "~/components"
 
 
 export function Dashboard() {
@@ -15,46 +15,9 @@ export function Dashboard() {
 
         <div>
 
-            <header className="bg-verde_claro text-white">
+            <Header />
 
-                <div className="container max-w-full p-2 flex justify-between items-center ">
-
-                    <img loading="lazy" src="/logo/Logo.png" alt="logo" className="w-16 sm:w-24 md:w-28 xl:w-30 2xl:w-40 " />
-
-                    <div className="flex gap-2 sm:gap-3 md:gap-4  items-center ">
-
-                        <img loading="lazy" src="/notification/notification.svg" alt="notification" className="w-2 sm:3 md:w-4 2xl:w-5" />
-
-                        <a href="/profile">
-                            <img loading="lazy" src="/user/user.svg" alt="user" className="w-2 sm:3 md:w-4 2xl:w-5" />
-                        </a>
-
-                        <span className=" font-semibold text-gray-950 text-sm sm:text-lg md:text-xl   " >Hola, Nutricionista</span>
-                    </div>
-
-                </div>
-
-            </header>
-
-            <nav className="container max-w-full ">
-
-                <div className="flex justify-around mt-5 mx-20 pb-4 border-b-2 border-verde_oscuro font-medium text-gris_texto text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl ">
-
-                    <div id="pagPrincipal" className="border-t-2 border-transparent hover:border-t-2 hover:border-verde_claro ">
-                        <a href="/dashboard" className="hover:text-gray-900"> Página principal</a>
-                    </div>
-
-                    <div id="consultante" className="border-t-2 border-transparent hover:border-t-2 hover:border-verde_claro">
-                        <a href="/dashboard" className="hover:text-gray-900" > Consultantes</a>
-                    </div>
-
-                    <div id="recetas" className="border-t-2 border-transparent hover:border-t-2 hover:border-verde_claro">
-                        <a href="/dashboard" className="hover:text-gray-900" > Recetas</a>
-                    </div>
-
-                </div>
-
-            </nav>
+            <NavBar />
 
 
             <main className="">
