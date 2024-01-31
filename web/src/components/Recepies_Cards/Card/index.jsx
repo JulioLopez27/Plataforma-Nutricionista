ad8b3759ad16f03ada65e48cf1646f677a076d61
Julio Lopez <julioneo95@hotmail.com>
2024-01-31 03:41:32 -0300

    Change: with

--- web/src/components/Recepies_Cards/Card/index.jsx
+++ web/src/components/Recepies_Cards/Card/index.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 export const Card = ({ children }) => {
     return (
-        <div className="bg-white shadow-md rounded-lg p-2 w-auto sm:p-4 sm:w-fit md:w-80">
+        <div className="bg-white shadow-md rounded-lg p-2 w-auto sm:p-4 sm:w-50 md:w-64 lg:w-72">
         {children}
     </div>
     )
