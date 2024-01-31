5c5987131f1ee3339ea7b319153bb194835410d3
Julio Lopez <julioneo95@hotmail.com>
2024-01-31 17:23:50 -0300

    Add: routes for consultante/recetas

--- web/src/pages/index.jsx
+++ web/src/pages/index.jsx
@@ -7,9 +7,9 @@ import {
 import { Home } from './Home'
 import { Signup } from './Signup'
 import { Dashboard } from './Dashboard'
-import { Profile } from "./Profile";
-
-
+import { Profile } from "./Profile"
+import { Consultante } from "./Consultante"
+import { Receta} from './Receta'
 
 const router = createBrowserRouter([
   {
@@ -26,6 +26,12 @@ const router = createBrowserRouter([
   }, {
     path: "/profile",
     element: <Profile />,
+  }, {
+    path: "/consultantes",
+    element: <Consultante />,
+  }, {
+    path: "/recetas",
+    element: <Receta />,
   },
 
 ]);
