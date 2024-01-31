1eb957dffbb6954fccef2675968bf0b3a82a7a54
Julio Lopez <julioneo95@hotmail.com>
2024-01-31 17:24:29 -0300

    Add files for consultantes/recetas

---
+++ web/src/pages/Receta/index.jsx
@@ -0,0 +1,9 @@
+
+export function Receta() {
+
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <img src="/public/img/error.jpg" loading="lazy" alt="" className="h-screen " />
+        </div>
+    )
+} 
