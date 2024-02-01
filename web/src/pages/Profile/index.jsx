b835f4c26a75fd1dce033d9502da6af3d223945b
Julio Lopez <julioneo95@hotmail.com>
2024-01-31 21:29:58 -0300

    fix

--- web/src/pages/Profile/index.jsx
+++ web/src/pages/Profile/index.jsx
@@ -5,7 +5,7 @@ export function Profile() {
 
     return (
         <div className="flex justify-center items-center h-screen">
-            <img src="/public/img/error.jpg" loading="lazy" alt="" className="h-screen " />
+            <img src="/public/assets/img/error.jpg" loading="lazy" alt="" className="h-screen " />
         </div>
     )
 } 
