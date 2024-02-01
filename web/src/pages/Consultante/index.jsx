acedaed06654083fcb6a12d7ba9703707c5c3291
Julio Lopez <julioneo95@hotmail.com>
2024-01-31 21:27:43 -0300

    Add error page

--- web/src/pages/Consultante/index.jsx
+++ web/src/pages/Consultante/index.jsx
@@ -4,7 +4,7 @@ export function Consultante() {
 
     return (
         <div className="flex justify-center items-center h-screen">
-            <img src="/public/img/error.jpg" loading="lazy" alt="" className="h-screen " />
+            <img src="/public/assets/img/error.jpg" loading="lazy" alt="" className="h-screen " />
         </div>
     )
 } 
--- web/src/pages/Receta/index.jsx
+++ web/src/pages/Receta/index.jsx
@@ -3,7 +3,7 @@ export function Receta() {
 
     return (
         <div className="flex justify-center items-center h-screen">
-            <img src="/public/img/error.jpg" loading="lazy" alt="" className="h-screen " />
+            <img src="/public/assets/img/error.jpg" loading="lazy" alt="" className="h-screen " />
         </div>
     )
 } 
