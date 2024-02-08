8f7b9814aced72a6dc14dd684eaee83a9a8b07e9
Julio Lopez <julioneo95@hotmail.com>
2024-02-08 02:51:17 -0300

    Se implementa authentication y boton de cerrar sesion

--- web/src/pages/Dashboard/index.jsx
+++ web/src/pages/Dashboard/index.jsx
@@ -1,21 +1,28 @@
-
+import { useLocalStorage, useAsyncFn } from 'react-use'
+import { Navigate,useNavigate } from 'react-router-dom'
 import { Recepies_Cards, Header, NavBar } from "~/components"
 
 
 export function Dashboard() {
+    const navigate = useNavigate()
 
+    const [auth,setAuth] = useLocalStorage('auth', {})
+    console.log(auth);
+    
+    if (!auth?.user?.id) {
+        return <Navigate to="/" replace={true} />
+    }
 
-    {/* {auth?.user?.id && (<div onClick={logout} className="p-2 cursor-pointer text-xl font-semibold"> */ }
-    {/* Logout */ }
-    {/* </div>) || (<div onClick={login} className="p-2 cursor-pointer text-xl font-bold "> */ }
-    {/* Login */ }
+    const logout = () => {
+        setAuth({})
+        navigate('/')
+    }
 
     return (
 
-
         <div>
 
-            <Header />
+            <Header/>
 
             <NavBar />
 
@@ -23,10 +30,15 @@ export function Dashboard() {
             <main>
 
                 <section id="header" className="max-w-full flex items-center justify-center">
+                   
                     <div className="flex flex-col text-center m-4 gap-4">
+                      
                         <h1 className="p-2 text-lg sm:text-xl md:text-3xl lg:text-4xl font-bold text-azul_titulo ">
                             Recetas sugeridas de ChefDigitales
                         </h1>
+                        {auth?.user?.id && (<div onClick={logout} className="p-2 cursor-pointer text-xl font-semibold">
+                        Logout
+                    </div>)}
                         <p className="w-full sm:w-5/6 md:w-3/4 lg:w-1/2 m-auto text-gray-900">
                             Bienvenido a nuestra plataforma de nutrición.
                             Aquí, podrás gestionar tus clientes, crear planes de alimentación personalizados,
