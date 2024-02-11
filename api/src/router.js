398bbae9d8f9a5b717867ac428510a6e1e029e13
Julio Lopez <julioneo95@hotmail.com>
2024-02-11 01:21:19 -0300

    Se estiliza las rutas

--- api/src/router.js
+++ api/src/router.js
@@ -8,12 +8,15 @@ export const router = new Router()
 
 
 
-router.post('/login', async (ctx) => { await Nutricionista.login(ctx) })
+router.get('/login', async (ctx) => { await Nutricionista.login(ctx) })
 router.post('/signup', async (ctx) => { await Nutricionista.signup(ctx) })
+
 router.get('/getSpecialty', async (ctx) => { await Nutricionista.getSpecialty(ctx) })
 router.get('/getCountries', async (ctx) => { await Nutricionista.getCountries(ctx) })
+
 router.get('/getProfileData', async (ctx) => { await Nutricionista.getProfileData(ctx) })
 router.put('/updateProfileData', async (ctx) => { await Nutricionista.updateProfile(ctx) })
+
 router.get('/getConsultants', async (ctx) => { await Nutricionista.getConsultantes(ctx) })
 router.get('/getHistory', async (ctx) => { await Nutricionista.getHistory(ctx) })
 router.get('/getHistoryInformes', async (ctx) => { await Nutricionista.getHistoryInformes(ctx) })
